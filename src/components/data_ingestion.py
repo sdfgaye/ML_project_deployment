d7f21e3f4d40ab393d69231974ad1b05165384f3
sdfgaye <souleymane.doudou.fall.gaye@gmail.com>
2024-10-05 15:12:44 +0000

    model trainer

--- src/components/data_ingestion.py
+++ src/components/data_ingestion.py
@@ -9,7 +9,10 @@ from sklearn.model_selection import train_test_split
 from dataclasses import dataclass
 
 from src.components.data_transformation import DataTransformation
-from src.utils import save_object
+from src.components.model_trainer import ModelTrainerConfig
+from src.components.model_trainer import ModelTrainer
+
+from src.utils import save_object, evaluate_models
 
 
 @dataclass
@@ -55,4 +58,7 @@ if __name__ == "__main__":
     train_data, test_data = obj.initiate_data_ingestion()
 
     data_transformation = DataTransformation()
-    data_transformation.initiate_data_transformation(train_data, test_data)
+    train_arr,test_arr,_=data_transformation.initiate_data_transformation(train_data,test_data)
+
+    modeltrainer=ModelTrainer()
+    print(modeltrainer.initiate_model_trainer(train_arr,test_arr))
